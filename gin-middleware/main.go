2287b228dad18605eb916bc87ee5e69c0a3e4a25
David Zhang <crispgm@gmail.com>
2021-10-15 18:10:57 +0800

    feat: add encryption example

--- gin-middleware/main.go
+++ gin-middleware/main.go
@@ -11,6 +11,10 @@ import (
 func main() {
 	r := gin.Default()
 	middleware := larkgin.NewLarkMiddleware()
+	middleware.BindURLPrefix("/lark")           // enable prefix binding if you need
+	middleware.WithEncryption("<encrypt-key>")  // enable encryption if you need
+	middleware.WithTokenVerification("<token>") // enable token verification if you need
+
 	larkGroup := r.Group("/lark")
 	larkGroup.Use(middleware.LarkChallengeHandler())
 	larkGroup.Use(middleware.LarkMessageHandler())
