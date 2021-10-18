39a44d01f342ade6e981b558a17cd90a079636fc
David Zhang <crispgm@gmail.com>
2021-10-18 10:57:42 +0800

    fix: reference to undefined method

--- at-message/main.go
+++ at-message/main.go
@@ -15,15 +15,15 @@ const (
 func main() {
 	bot := lark.NewChatBot(appID, appSecret)
 	bot.GetTenantAccessTokenInternal(true)
-	userInfo, _ := bot.GetUserIDByEmail("xxxxxx@example.com")
+	openID := "ou_xxxxxxx" // you may get this from incoming message or other API
 	mb := lark.NewMsgBuffer(lark.MsgText)
 	// Method 1
-	text := fmt.Sprintf("AT Message 1 <at user_id=\"%s\">%s</at>", userInfo.OpenID, "name")
+	text := fmt.Sprintf("AT Message 1 <at user_id=\"%s\">%s</at>", openID, "name")
 	msg1 := mb.BindOpenChatID("oc_xxxxxx").Text(text).Build()
 	bot.PostMessage(msg1)
 	mb.Clear()
 	// Method 2
-	tb2 := lark.NewTextBuilder().Text("AT Message 2").Mention(userInfo.OpenID)
+	tb2 := lark.NewTextBuilder().Text("AT Message 2").Mention(openID)
 	msg2 := mb.BindOpenChatID("oc_xxxxxx").Text(tb2.Render()).Build()
 	bot.PostMessage(msg2)
 	mb.Clear()
